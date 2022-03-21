7e3b869ad84dd150ceff84931e7dde31a7e1f286
Maria Hinge Pedersen <mariahingep@Marias-MacBook-Air.local>
2022-03-21 12:42:22 +0100

    exercise done

---
+++ exercises/passf/table.cs
@@ -0,0 +1,8 @@
+using System;
+using static System.Console;
+public class table{
+	public static void make_table(Func<double, double> f, double a, double b, double dx){
+		for(double x=a; x<=b; x+=dx) WriteLine($"{x} {f(x)}");
+	}
+}
+
