ffa93fbb8d95cbdd886214be8961380c94fc73b1
Maria Hinge Pedersen <mariahingep@Marias-MacBook-Air.local>
2022-04-02 12:44:07 +0200

    C fixed

--- homeworks/inputoutput/C/fileio.cs
+++ homeworks/inputoutput/C/fileio.cs
@@ -7,7 +7,7 @@ class main{
 		string infile=null, outfile=null;
 		foreach(var arg in args){
 			var words=arg.Split(':');
-			if(words[0]=="'input")infile=words[1];
+			if(words[0]=="-input")infile=words[1];
 			else if(words[0]=="-output")outfile=words[1];
 			else { Error.WriteLine("wrong argument"); return 1; }
 		}
