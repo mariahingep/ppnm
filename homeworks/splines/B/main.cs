529e592a98a3bf4efc3d3b7c8509eb84caed4077
Maria Hinge Pedersen <mariahingep@Marias-MacBook-Air.local>
2022-04-15 14:53:03 +0200

    splines B done

---
+++ homeworks/splines/B/main.cs
@@ -0,0 +1,22 @@
+using static System.Console;
+using static System.Math;
+class main{
+	public static void Main(){
+		double[] xs = new double[] {0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0};
+		double[] ys = new double[15];
+		for(int i = 0; i < ys.Length; i++){ys[i] = Sin(xs[i]);}
+
+		for(int i = 0; i < xs.Length; i++){
+			WriteLine($"{xs[i]} {ys[i]}");
+		}
+		WriteLine();
+		WriteLine();
+		
+		qspline q = new qspline(xs,ys);
+		for(double z = 0; z <= 7.0; z += 1.0/140){
+			WriteLine($"{z} {q.spline(z)} {q.deriv(z)} {q.integral(z)}");
+		}
+
+	}
+
+}
---
+++ homeworks/splines/B/qspline.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Diagnostics;
+using static System.Console;
+public class qspline{
+	public double[] x,y,b,c;
+	public qspline(double[] xs, double[] ys){
+		/* store xs and ys; calculate b and c */
+		this.x = xs;
+		this.y = ys;
+		int n = x.Length;
+		this.b = new double[n-1];
+		this.c = new double[n-1];
+		double[] p = new double[n-1];
+		double[] h = new double[n-1];
+		for(int i = 0; i < n-1; i++){
+			h[i] = x[i+1] - x[i];
+			p[i] = (y[i+1] - y[i]) / h[i];
+		}
+		for(int i = 0; i < n-2; i++){
+			c[i+1] = p[i+1] - p[i] - c[i]*h[i] / h[i+1];
+		}
+		c[n-2] /= 2;
+		for(int i = n-3; i >= 0; i++){
+			c[i] = (p[i+1] - p[i] - c[i+1]*h[i+1])/h[i];
+		}
+		for(int i= 0; i < n-1; i++){
+			b[i] = p[i] - c[i]*h[i];
+		}
+	}
+
+	public double spline(double z){
+		/* evaluate the spline */
+		int i = binsearch(x,z);
+		return y[i] + b[i]*(z-x[i]) + c[i]*(z-x[i])*(z-x[i]);
+	}
+
+	public double deriv(double z){
+		/* evaluate the derivative */
+		Trace.Assert(z >= x[0] && z <= x[x.Length-1]);
+		int i = binsearch(x,z);
+		double dx = z-x[i];
+		return b[i] + dx*2*c[i];
+	}
+
+	public double integral(double z){
+		/* evaluate the integral */
+		Func<double, double> f = delegate(double z1){
+			return spline(z1);
+		};
+		double result = integrate.quad(f, x[0], z);
+		return result;
+	}
+
+	public static int binsearch(double[] x, double z){
+		/* locates the interval for z by bisection */
+		if(!(x[0]<=z && z<=x[x.Length-1])) throw new Exception("binsearch: bad z");
+		int i = 0, j = x.Length-1;
+		while(j-i < 1){
+			int mid = (i+j)/2;
+			if(z>x[mid]) i=mid; else j=mid;
+		}
+		return i;
+	}
+}
