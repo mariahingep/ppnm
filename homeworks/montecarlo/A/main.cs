3b16d9171c6172bee46224480a2a9388316b2ce5
Maria Hinge Pedersen <mariahingep@Marias-MacBook-Air.local>
2022-04-25 09:34:53 +0200

    monte carlo A done

---
+++ homeworks/montecarlo/A/main.cs
@@ -0,0 +1,36 @@
+using System;
+using static System.Console;
+using static System.Math;
+public class main{
+	public static void Main(){
+		WriteLine("Test of Monte Carlo implementation on a few integrals\n");
+		WriteLine("(Examples from Wikipedia) The integral of x^2 + 4y with xlims = [11,14] and ylims = [7,10]:");
+
+		Func<vector, double> f1 = xy => xy[0]*xy[0]+4*xy[1];
+		vector a1 = new double[] {11,7};
+		vector b1 = new double[] {14,10};
+		var res1 = mc.plainmc(f1, a1, b1, 100000);
+		
+		WriteLine($"Result is {res1.Item1} with the error {res1.Item2}");
+		WriteLine("The result should be 1719\n");
+
+		WriteLine("The integral of x^2/(1+y^2) with xlims = [0,1] and ylims = [0,1]:");
+		Func<vector, double> f2 = xy => xy[0]*xy[0]/(1+xy[1]*xy[1]);
+		vector a2 = new double[] {0,0};
+		vector b2 = new double[] {1,1};
+		var res2 = mc.plainmc(f2, a2, b2, 100000);
+
+		WriteLine($"Result is {res2.Item1} with the error {res2.Item2}");
+		WriteLine("The result should be {PI/12}\n");
+
+		WriteLine("The integral of 1/(pi^3*{1-cos(x)*cos(y)*cos(z)}) with xlims = ylims = zlims = [0,pi]:");
+		Func<vector, double> f3 = xyz => 1.0/(Pow(PI,3)*(1.0-Cos(xyz[0])*Cos(xyz[1])*Cos(xyz[2])));
+		vector a3 = new double[] {0,0,0};
+		vector b3 = new double[] {PI,PI,PI};
+		var res3 = mc.plainmc(f3, a3, b3, 100000);
+		
+		WriteLine($"Result is {res3.Item1} with the error {res3.Item2}");
+		WriteLine("The result should be 1.393203929668...");
+	}
+
+}
